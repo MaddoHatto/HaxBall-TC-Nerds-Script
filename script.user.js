a74b5d564f7cc737f577e4a983478fda2eaf4d1d
MaddoHatto <73061096+MaddoHatto@users.noreply.github.com>
2021-05-19 17:55:28 +0200

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -2,7 +2,7 @@
 // @name         HaxBall TC Nerds script
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
+// @description  HaxBall TC Nerds script
 // @author       MaddoHatto
 // @source       https://github.com/MaddoHatto/HaxBall-TC-Nerds-Script
 // @match        https://www.haxball.com/headless
